d462a9b26d0579ae7f6339f677da4db4ecf597ab
Marvi <mmuchany@vols.utk.edu>
2023-11-29 03:20:44 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="area_calculator",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here, for example:
+        # 'numpy',
+    ],
+    python_requires=">=3.6",
+)
